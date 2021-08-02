a0d594d629504eefd8243652cf97df0444c758a9
shu8hampanwar <59318241+shu8hampanwar@users.noreply.github.com>
2021-08-02 17:42:24 +0530

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -1,16 +1,20 @@
 
 
-const input = document.getElementById('email')
+const success = document.getElementById('subscribe-success')
 
 const form = document.getElementById('form')
+
 
 
 form.addEventListener("submit", (e) => {
 
     form.style.display = 'none';
 
+    success.style.display = 'block';
+
     e.preventDefault()
 
 })
+
 
 
