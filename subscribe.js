8f23437fa295b5e309c32079c80d4290154fee48
shu8hampanwar <59318241+shu8hampanwar@users.noreply.github.com>
2021-08-02 17:29:14 +0530

    Add files via upload

---
+++ subscribe.js
@@ -0,0 +1,16 @@
+
+
+const input = document.getElementById('email')
+
+const form = document.getElementById('form')
+
+
+form.addEventListener("submit", (e) => {
+
+    form.style.display = 'none';
+
+    e.preventDefault()
+
+})
+
+
